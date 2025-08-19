821050ce2425e0828106ba1843742a498924f271
bag31 <sehuibag31@sookmyung.ac.kr>
2025-08-19 16:05:28 +0900

    Initial commit

---
+++ lib/screen/button_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_projects/screen/container/container_practice_screen.dart';
+import 'package:flutter_projects/screen/container/container_screen.dart';
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // onPressed: 클릭했을 때 중괄호 안으로 이동
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ContainerScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Container"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ContainerPracticeScreen();
+                    },
+                  ),
+                );
+              },
+              child: Text("Container Practice"),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: () {}, child: Text("button3")),
+          ],
+        ),
+      ),
+    );
+  }
+}
