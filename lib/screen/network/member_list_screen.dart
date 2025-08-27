2708137586d9867409b95750a9f55be08545d3c2
bag31 <sehuibag31@sookmyung.ac.kr>
2025-08-27 14:32:15 +0900

    CircularProgressIndicator

--- lib/screen/network/member_list_screen.dart
+++ lib/screen/network/member_list_screen.dart
@@ -12,7 +12,7 @@ class MemberListScreen extends StatefulWidget {
 }
 
 class _MemberListScreenState extends State<MemberListScreen> {
-  Dio dio = Dio(BaseOptions(baseUrl: "https://d0a701c36a9e.ngrok-free.app"));
+  Dio dio = Dio(BaseOptions(baseUrl: "https://0f5d227aa566.ngrok-free.app"));
   Dio dio2 = Dio(
     BaseOptions(
       baseUrl:
@@ -21,28 +21,42 @@ class _MemberListScreenState extends State<MemberListScreen> {
   );
 
   List<Member> memberList = [];
+  bool loading = false;
 
   // https://online-lecture-data.s3.ap-northeast-2.amazonaws.com/data.json
 
   @override
   Widget build(BuildContext context) {
+    if(loading) {
+      return Scaffold(body: CircularProgressIndicator(),);
+    }
     return Scaffold(
       appBar: AppBar(title: Text("멤버 조회")),
       body: Column(
         children: [
           ElevatedButton(
-            onPressed: () {
+            onPressed: () async {
+              loading = true;
+              setState(() {});
+              var response = await dio.get("/api/v1/member/all");
+
+              memberList = (response.data as Iterable)
+                  .map((e) => Member.fromJson(e))
+                  .toList();
+              loading = false;
+              setState(() {});
+
               // baseURL에 맞춰 /를 잘 써야 함. 보통 baseURL 마지막에 / 없이 쓴다.
-              dio.get("/api/v1/member/all").then((value) {
-                // if(value.data is Iterable) {}
-                memberList = (value.data as Iterable).map((e) {
-                  return Member.fromJson(
-                    e,
-                  ); // Iterable인 value(여러 개 Json 데이터가 있음)를 map이 하나씩 꺼내서 e에 넣음. 그리고 각각을 Member.fromJson 꼴을 적용해서 Member 객체 리스트로 만든다.
-                }).toList();
-                setState(() {});
-                print('memberList : $memberList');
-              });
+              // dio.get("/api/v1/member/all").then((value) {
+              //   // if(value.data is Iterable) {}
+              //   memberList = (response.data as Iterable).map((e) {
+              //     return Member.fromJson(
+              //       e,
+              //     ); // Iterable인 value(여러 개 Json 데이터가 있음)를 map이 하나씩 꺼내서 e에 넣음. 그리고 각각을 Member.fromJson 꼴을 적용해서 Member 객체 리스트로 만든다.
+              //   }).toList();
+              //   setState(() {});
+              //   print('memberList : $memberList');
+              // });
             },
             child: Text("Get Data"),
           ),
